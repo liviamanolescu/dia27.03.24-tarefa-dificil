603010f606552581f39b3040ed7af5378458faa1
Livia Ibmec <159266742+liviamanolescu@users.noreply.github.com>
2024-03-27 11:27:18 -0300

    Add files via upload

---
+++ liviaexemploprova_semana 7_aula270324.py
@@ -0,0 +1,22 @@
+def funcao1(a):
+    return sum(a), sum(a)/len(a)
+
+def funcao2 (a,b,c):
+    for i in range (len(a)):
+        if a[i] == b:
+            a[i] = c
+    return a
+
+def gerar_triangulo (num_linhas):
+    for i in range (1, num_linhas + 1):
+        print ('*' * i)
+        
+
+
+
+
+    
+
+    
+    
+
